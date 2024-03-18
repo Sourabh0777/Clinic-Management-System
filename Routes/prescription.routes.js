148768a796c46b09e903d50911bce51167f1bcc5
vikas4real <vikas000pandey@gmail.com>
2024-03-18 13:25:27 +0530

    updated prescription route

--- Routes/prescription.routes.js
+++ Routes/prescription.routes.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const { verifyIsLoggedIn } = require("../middleware/verifyAuthToken");
 const {
    getPrescriptions,
 } = require("../Controller/Prescription/PrescriptionController");
 const router = express.Router();
-router.use(verifyIsLoggedIn);
 router.post("/", getPrescriptions);
 
 module.exports = router;
