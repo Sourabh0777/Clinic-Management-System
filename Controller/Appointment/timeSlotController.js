daac435f2740126febf58708553a773dae6795dc
Sourabh <sourabh.verma9770@gmail.com>
2024-03-28 15:59:00 +0530

    Error Sent for getTimeSlotDetails

--- Controller/Appointment/timeSlotController.js
+++ Controller/Appointment/timeSlotController.js
@@ -1,15 +1,17 @@
-const TimeSlot = require("../../Models/TimeSlotModel");
+const TimeSlot = require('../../Models/TimeSlotModel');
+const HttpError = require('../../Models/http-error');
 const getTimeSlotDetails = async (req, res, next) => {
   try {
     const { id } = req.params;
     const timeSlot = await TimeSlot.findById(id);
     if (!timeSlot) {
-      const err = new HttpError("No time slot was found by this id.", 500);
+      const err = new HttpError('No time slot was found by this id.', 500);
+      throw err;
     }
     return res.send(timeSlot);
   } catch (error) {
-    const err = new HttpError("Unable to find time slot details", 500);
-    return next(error || err);
+    const err = new HttpError('Unable to find time slot details', 500);
+    return next(err);
   }
 };
 module.exports = { getTimeSlotDetails };
